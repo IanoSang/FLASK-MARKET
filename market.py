5194dab2b512f417e39ff961e101a6d516675df4
IanoSang <ianosang18@gmail.com>
2022-05-01 00:59:34 +0300

    adds a virtual env and reinstall flask to the app

--- market.py
+++ market.py
@@ -1,6 +1,10 @@
+import sqlalchemy as sqlalchemy
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
