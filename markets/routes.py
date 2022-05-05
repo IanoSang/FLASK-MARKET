12f53720fd98800232dcfbc575ba1b23531fda85
IanoSang <ianosang18@gmail.com>
2022-05-06 00:45:55 +0300

    feat: adds a route url and redirect when the users sign up

--- markets/routes.py
+++ markets/routes.py
@@ -1,7 +1,8 @@
 from markets import app
-from flask import render_template
-from markets.models import Item
+from flask import render_template, redirect, url_for
+from markets.models import Item, User
 from markets.forms import RegisterForm
+from markets import db
 
 
 @app.route('/')
@@ -20,7 +21,13 @@ def market_page():
 # def about_page(username):
 #     return f"<h2>About {username}</h2>"
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        use_to_create = User(username=form.username.data,
+                             email_address=form.email_address.data, password_hash=form.password1.data)
+        db.session.add(use_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
     return render_template('register.html', form=form)
