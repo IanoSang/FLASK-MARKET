2a86b233e686969bda7d7688d7664815c6415d83
IanoSang <ianosang18@gmail.com>
2022-05-06 00:42:38 +0300

    feat: adds the initiolization of the database

--- markets/__init__.py
+++ markets/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'ed96823c766c274dc3bd19e0'
 db = SQLAlchemy(app)
 
 from markets import routes
